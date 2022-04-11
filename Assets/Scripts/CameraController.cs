e4615f47a7aa694dca6c1be10aba656256c0512a
Justin Roderman <jcroderman@gmail.com>
2022-04-11 00:35:24 -0700

    Adds terrain and free cam

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public float mouseSensitivity;
+
+    private float xRotation;
+
+    private void Start() {
+        Cursor.lockState = CursorLockMode.Locked;
+        xRotation = transform.localEulerAngles.x;
+    }
+
+    private void Update() {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90, 90);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.parent.transform.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class MovementController : MonoBehaviour {
+    public float baseSpeed;
+
+    private CharacterController controller;
+
+    void Start() {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update() {
+        float x = Input.GetAxis("MovementX");
+        float y = Input.GetAxis("MovementY") / 2; // Cut in half so we ascend/descend slower
+        float z = Input.GetAxis("MovementZ");
+
+        Vector3 move = transform.right * x + transform.up * y + transform.forward * z;
+
+        float speed = baseSpeed;
+        if (Input.GetButton("Speed")) {
+            speed *= 2;
+        }
+
+        controller.Move(move * speed * Time.deltaTime);
+    }
+}
